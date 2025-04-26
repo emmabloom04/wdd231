389e1ac596e979dd07385935bb81dc5a11ff6aec
emmabloom04 <blo23006@byui.edu>
2025-04-26 00:01:35 -0600

    adding a little bit of javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a")
+disclaimer.href = parkData.url;
+disclaimer.innerHtml = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner-title">${info.name}</a>
+    <p class="hero-banner-subtitle"
+    <span>${info.destination}</span>
+    <span>${info.states}</span>
+    </p>`;
+}
