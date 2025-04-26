63cedfbfb0ca3931704ae717106f54435bb9bcf0
emmabloom04 <blo23006@byui.edu>
2025-04-26 13:23:49 -0600

    finishing up nps 1 javascript

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,25 @@ import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
 
-const disclaimer = document.querySelector(".disclaimer > a")
-disclaimer.href = parkData.url;
-disclaimer.innerHtml = parkData.fullName;
-
 function parkInfoTemplate(info) {
     return `<a href="/" class="hero-banner-title">${info.name}</a>
     <p class="hero-banner-subtitle"
-    <span>${info.destination}</span>
+    <span>${info.designation}</span>
     <span>${info.states}</span>
     </p>`;
 }
+
+const disclaimer = document.querySelector(".disclaimer a")
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+const pageTitle = document.querySelector("title");
+pageTitle.innerText = parkData.name;
+
+const heroBanner = document.querySelector(".hero-banner-content");
+heroBanner.innerHTML = parkInfoTemplate(parkData);
+
+const image = parkData.images[0];
+const bannerImg = document.querySelector(".hero-banner img");
+bannerImg.alt = image.altText;
+bannerImg.src = image.url;
