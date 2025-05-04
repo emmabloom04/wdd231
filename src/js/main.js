510de099f1e7e562dbb2d3e9da5db1d04ef7cd33
emmabloom04 <blo23006@byui.edu>
2025-05-03 23:10:16 -0600

    working on nps 2

--- src/js/main.js
+++ src/js/main.js
@@ -4,24 +4,40 @@ import { getParkData } from "./parkService.mjs";
 const parkData = getParkData();
 
 function parkInfoTemplate(info) {
-    return `<a href="/" class="hero-banner-title">${info.name}</a>
-    <p class="hero-banner-subtitle">
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
     <span>${info.designation}</span>
     <span>${info.states}</span>
     </p>`;
 }
 
-const disclaimer = document.querySelector(".disclaimer a")
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
 
-const pageTitle = document.querySelector("title");
-pageTitle.innerText = parkData.name;
+function setHeaderInfo(data) {
+    const disclaimer = document.querySelector(".disclaimer a")
+    disclaimer.href = data.url;
+    disclaimer.innerHTML = data.fullName;
 
-const heroBanner = document.querySelector(".hero-banner-content");
-heroBanner.innerHTML = parkInfoTemplate(parkData);
+    const pageTitle = document.querySelector("title");
+    pageTitle.innerText = data.name;
 
-const image = parkData.images[0];
-const bannerImg = document.querySelector(".hero-banner img");
-bannerImg.alt = image.altText;
-bannerImg.src = image.url;
+    const heroBanner = document.querySelector(".hero-banner__content");
+    heroBanner.innerHTML = parkInfoTemplate(data);
+
+    const image = data.images[0];
+    const bannerImg = document.querySelector(".hero-banner img");
+    bannerImg.alt = image.altText;
+    bannerImg.src = image.url;
+}
+
+const intro = document.querySelector(".intro");
+intro.innerHTML = `<h1>${parkData.fullName}</h1>
+    <p>${parkData.description}</p>`
+const info = document.querySelector(".info");
+
+function mediaCardTemplate(info) {
+    return `<img src="" alt=""><img/>
+        <h2></h2>
+        <p></p>`
+}
+
+setHeaderInfo(parkData);
