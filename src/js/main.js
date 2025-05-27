f0c6809c1b4419fafb5faa93a94fecb6d78659c6
emmabloom04 <blo23006@byui.edu>
2025-05-27 11:00:55 -0600

    adding files for first subpage

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ import { getInfoLinks, getParkData } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
-function setParkIntro(data) {
+export function setParkIntro(data) {
   const intro = document.querySelector(".intro");
   intro.innerHTML = `<h1>${data.fullName}</h1>
         <p>${data.description}</p>`;
 }
 
-function setParkInfoLinks(data) {
+export function setParkInfoLinks(data) {
   const info = document.querySelector(".info");
   const html = data.map(mediaCardTemplate);
   info.insertAdjacentHTML("afterbegin", html.join(""));
