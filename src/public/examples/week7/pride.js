7cffec8970a70af8e47e77d3050ffc60a048b909
emmabloom04 <blo23006@byui.edu>
2025-06-10 12:36:15 -0600

    finishing pride and prejudice quotes

--- src/public/examples/week7/pride.js
+++ src/public/examples/week7/pride.js
@@ -1,23 +1,62 @@
 const button = document.querySelector(".quote-button");
 
-function loadQuotes(file_name) {
-    fetch(file_name)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("HTTP error " + response.status);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error("Fetch error:", error);
-        })
+const alreadyLoaded = CheckQuotes();
+let quotes = null;
+
+if (alreadyLoaded) {
+  quotes = LoadQuotesFromStorage();
+  LoadQuotes(quotes);
+}
+
+async function LoadQuotes(quotes) {
+  button.classList.toggle("hidden");
+
+  quotes = await LoadQuotesFromJSON("quotes.json");
+  StoreQuotes(quotes);
+  const quoteList = QuoteList(quotes);
+  const quoteBox = document.querySelector(".quote-box");
+  quoteBox.innerHTML = quoteList;
+}
+
+function QuoteList(quotes) {
+  const quoteList = quotes.map((quote) => QuotesTemplate(quote)).join("");
+  return quoteList;
+}
+
+function StoreQuotes(quotes) {
+  localStorage.setItem("quotes", JSON.stringify(quotes));
+  localStorage.setItem("quotesLoaded", JSON.stringify(true));
+}
+
+async function LoadQuotesFromJSON(file_name) {
+  let data = {};
+  const response = await fetch(file_name);
+  if (response.ok) {
+    data = await response.json();
+    console.log(data);
+  } else throw new Error("response not ok");
+  const quotes = data["books"][0]["quotes"];
+  return quotes;
+}
+
+function LoadQuotesFromStorage() {
+  const quotes = JSON.parse(localStorage.getItem("quotes"));
+  return quotes;
+}
+
+function CheckQuotes() {
+  const alreadyLoaded = JSON.parse(localStorage.getItem("quotesLoaded"));
+  return alreadyLoaded;
 }
 
 function QuotesTemplate(quote) {
-    return `<li>${quote}</li>`
+  return `<li>${quote}</li>`;
 }
 
-button.addEventListener("click", loadQuotes('quotes.json'));
+button.addEventListener("click", async () => {
+  try {
+    const quotesData = await LoadQuotes();
+  } catch (error) {
+    console.error("Failed to load quotes:", error);
+  }
+});
